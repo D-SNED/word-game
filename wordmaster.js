159b36ceed987159c714fd6b55f28a2d7ef7b453
DSnediker <dsnediker7@gmail.com>
2024-11-12 12:24:46 -0800

    got word of day and added function that hides loading symbol

--- wordmaster.js
+++ wordmaster.js
@@ -1,14 +1,57 @@
 const letters = document.querySelectorAll('.square');
 const loadingDiv = document.querySelector('.info-bar');
-
+const ANSWER_LENGTH = 5;
 
 async function init() {
+    let currentGuess = '';
+    let currentRow = 0;
+
+    const res = await fetch('https://words.dev-apis.com/word-of-the-day');
+    const resObj = await res.json();
+    const word = resObj.word.toUpperCase();
+    setLoading(false);
+
+    console.log(word);
+
+
+    function addLetter (letter) {
+        if (currentGuess.length < ANSWER_LENGTH) {
+            // add letter to the end
+            currentGuess += letter;
+        } else {
+            // replace last letter
+            currentGuess = currentGuess.substring(0, currentGuess.length - 1) + letter
+        }
+
+        letters[ANSWER_LENGTH * currentRow + currentGuess.length - 1].innerText = letter;
+    }
+
+    async function commit() {
+        if (currentGuess.length != ANSWER_LENGTH) {
+            // do nothing
+            return;
+        }
+
+        // TODO validate word
+
+        // TODO do marking as "correct" "close" or "wrong"
+
+        // TODO did they win or lose?
+
+        currentRow ++;
+        currentGuess = '';
+    }
+
+    function backspace() {
+        currentGuess = currentGuess.substring(0, currentGuess.length - 1);
+        letters[ANSWER_LENGTH * currentRow + currentGuess.length].innerText = '';
+    }
 
 
     document.addEventListener('keydown', function handleKeyPress (event) {
         const action = event.key;
 
-        console.log(action);
+
 
         if (action === 'Enter') {
             commit();
@@ -20,6 +63,14 @@ async function init() {
             // do nothing
         }
     })
+}
+
+function isLetter(letter) {
+    return /^[a-zA-Z]$/.test(letter);
+}
+
+function setLoading(isLoading) {
+    loadingDiv.classList.toggle('hidden', !isLoading);
 }
 
 init();
