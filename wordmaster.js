933182bf7ec91e594296a8d8a2214bd0dfc6c3d1
DSnediker <dsnediker7@gmail.com>
2024-11-14 13:27:40 -0800

    added to many things.. validate word, winning animation, etc..

--- wordmaster.js
+++ wordmaster.js
@@ -38,7 +38,24 @@ async function init() {
             return;
         }
 
-        // TODO validate word
+        isLoading = true;
+        setLoading(true);
+        const res = await fetch("https://words.dev-apis.com/validate-word", {
+            method: "POST",
+            body: JSON.stringify({ word: currentGuess})
+        });
+
+        const resObj = await res.json();
+        const validWord = resObj.validWord;
+        // const { validWord } = resObj;
+
+        isLoading = false;
+        setLoading(false);
+
+        if (!validWord) {
+            markInvalidWord();
+            return
+        }
 
         const guessParts = currentGuess.split("");
         const map = makeMap(wordParts);
@@ -72,6 +89,7 @@ async function init() {
             //win
 
             alert("You Win!");
+            document.querySelector('.brand').classList.add('winning');
             done = true;
             return;
 
@@ -86,6 +104,18 @@ async function init() {
     function backspace() {
         currentGuess = currentGuess.substring(0, currentGuess.length - 1);
         letters[ANSWER_LENGTH * currentRow + currentGuess.length].innerText = '';
+    }
+
+    function markInvalidWord() {
+        //alert("Not a valid word");
+        for(let i = 0; i < ANSWER_LENGTH; i++){
+            letters[currentRow * ANSWER_LENGTH + i].classList.remove('invalid');
+
+            setTimeout(function () {
+                letters[currentRow * ANSWER_LENGTH + i].classList.add('invalid');
+            }, 10);
+        }
+
     }
 
 
